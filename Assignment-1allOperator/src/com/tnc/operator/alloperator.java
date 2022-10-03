f70d9376e14e29dfb6219d7a807b2cf344941dbf
Syed Salauddin Ayyub <Syed Salauddin Ayyub@DESKTOP-SULTAN>
2022-10-03 12:16:12 +0530

    All Operator

---
+++ Assignment-1allOperator/src/com/tnc/operator/alloperator.java
@@ -0,0 +1,19 @@
+package com.tnc.operator;
+
+public class alloperator {
+
+	public static void main(String[] args) 
+	{
+		int a = 10;
+	      int b = 20;
+
+	      System.out.println("10 == 20 = " + (a == b) );
+	      System.out.println("10 != 20 = " + (a != b) );
+	      System.out.println("10 > 20 = " + (a > b) );
+	      System.out.println("10 < 20 = " + (a < b) );
+	      System.out.println("10 >= 20 = " + (b >= a) );
+	      System.out.println("20 <= 10 = " + (b <= a) );
+		
+	}
+
+}
