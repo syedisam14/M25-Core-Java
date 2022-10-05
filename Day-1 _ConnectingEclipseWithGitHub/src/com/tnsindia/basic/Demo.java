a35a08968789be33d8d35e9836f55a75e8f4270e
Syed Salauddin Ayyub <Syed Salauddin Ayyub@DESKTOP-SULTAN>
2022-10-05 12:22:34 +0530

    Day-1 Commit

---
+++ Day-1 _ConnectingEclipseWithGitHub/src/com/tnsindia/basic/Demo.java
@@ -0,0 +1,11 @@
+package com.tnsindia.basic;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int x=143;
+		System.out.println(x);
+
+	}
+
+}
---
+++ Day-1 _ConnectingEclipseWithGitHub/src/com/tnsindia/basic/TestingEclipseForGitHub.java
@@ -0,0 +1,10 @@
+package com.tnsindia.basic;
+
+public class TestingEclipseForGitHub {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to TNS India Foundation with M25 Batch");
+	}
+
+}
