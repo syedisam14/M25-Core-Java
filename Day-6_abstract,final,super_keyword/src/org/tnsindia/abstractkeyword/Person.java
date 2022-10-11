b7dd797666efee052ec42fb347585220678980db
Syed Salauddin Ayyub <Syed Salauddin Ayyub@DESKTOP-SULTAN>
2022-10-11 12:59:02 +0530

    First Commit

---
+++ Day-6_abstract,final,super_keyword/src/org/tnsindia/abstractkeyword/Person.java
@@ -0,0 +1,29 @@
+package org.tnsindia.abstractkeyword;
+
+public abstract class Person {
+	private String name;
+	private String gender;
+	// parameterized constructor
+	public Person(String name, String gender) {
+		super();
+		this.name = name;
+		this.gender = gender;
+	}
+	//abstract method
+	public abstract void work();
+	
+	//toString method
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", gender=" + gender + "]";
+	} 
+	
+	//user-defined method  for change the name
+	public void changeName(String newName)
+	{
+		name=newName;
+	}
+	
+	
+
+}
