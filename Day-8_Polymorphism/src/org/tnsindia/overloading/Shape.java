6d570c74294560c2d234e21c7b8bc74470cc89b3
Syed Salauddin Ayyub <Syed Salauddin Ayyub@DESKTOP-SULTAN>
2022-10-11 13:00:57 +0530

    First Commit

---
+++ Day-8_Polymorphism/src/org/tnsindia/overloading/Shape.java
@@ -0,0 +1,34 @@
+package org.tnsindia.overloading;
+
+class Shape {
+	//method overloading
+	//zero-arg
+	public void area()
+	{
+		System.out.println("Find area:");
+	}
+	//one-arg
+	public void area(int r)
+	{
+		System.out.println("Area of Circle:"+(3.14*r*r));
+	}
+	//two-arg
+	public void area(int l,int b)
+	{
+		System.out.println("Area of Rectangle:"+(l*b));
+	}
+	
+	
+}
+/*for method overloading child class is not required*/
+public class ExampleOnMethodOverloading {
+
+	public static void main(String[] args) {
+		Shape s=new Shape();
+		s.area();
+		s.area(2);
+		s.area(10, 15);
+
+	}
+
+}
