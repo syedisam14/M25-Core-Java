908e4906286fadd7132f91bdd6d41960e5ab986d
Syed Salauddin Ayyub <Syed Salauddin Ayyub@DESKTOP-SULTAN>
2022-11-07 16:28:27 +0530

    First commit

---
+++ Day-17_JavaAnnotations/src/org/tnsindia/annotations/OverrideAnnotationDemo.java
@@ -0,0 +1,29 @@
+package org.tnsindia.annotations;
+
+class Parent
+{
+	public void print()
+	{
+		System.out.println("Parent class method");
+	}
+}
+
+class Child extends Parent
+{
+	@Override
+	public void print()
+	{
+		super.print();
+		System.out.println("Child class method");
+	}
+	
+}
+public class OverrideAnnotationDemo {
+
+	public static void main(String[] args) {
+		Child p=new Child();
+		p.print();
+
+	}
+
+}
